13fd3ecd7dabb9e792982011cd296a85de4c84cb
LiJohnson <598420668@163.com>
2015-01-05 23:07:51 +0800

    add sample module

---
+++ sample/src/main/java/com/joanzapata/android/icons/sample/ListIconActivity.java
@@ -0,0 +1,13 @@
+package com.joanzapata.android.icons.sample;
+
+import android.support.v7.app.ActionBarActivity;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+
+@EActivity(R.layout.activity_list_icon)
+@OptionsMenu(R.menu.menu_list_icon)
+public class ListIconActivity extends ActionBarActivity {
+
+}
